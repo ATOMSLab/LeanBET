34a647d095a3258870462c38cd628dbb61c26fbc
Ejike <54361715+davoagk@users.noreply.github.com>
2026-01-21 21:43:53 -0500

    Update BET-CSV-examples.lean
    Delete

--- BET-CSV-examples.lean
+++ BET-CSV-examples.lean
@@ -1,11 +1,1 @@
-import BET.StreamRead
 
-def CSV_LRM_Model (args : List String) : IO UInt32 := main args
-
-
-#eval CSV_LRM_Model [
-  "AdsorptionData/HKUST-1_tposed.csv",
-  "AdsorptionData/HKUST-1.csv",
-  "AdsorptionData/NU-1104.csv",
-  "AdsorptionData/NU-1104_For_LRM.csv"
-  ]
